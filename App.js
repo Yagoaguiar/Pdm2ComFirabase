c168351156a51a2c6f0b74525806c4f4ee392bc4
Yago Aguiar <95005910+Yagoaguiar@users.noreply.github.com>
2023-11-25 01:08:21 -0300

    tela de editar produto foi criada close#21

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
+import Editar from './screens/Editar';
 import Home from './screens/Home';
 import Login from './screens/Login';
 import NovaCompra from './screens/NovaCompra';
 import Register from './screens/Register';
 
 const App = () => {
-    return <Home/>;
+    return <Editar/>;
   }
   export default App;
