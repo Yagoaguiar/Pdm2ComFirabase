6cca5d404cb7d142f3c728ee5db2fbe998422837
Eduardo Fernandes <iamedusm@gmail.com>
2023-11-28 15:07:14 -0300

    Criação da validação do login

--- routes/MainNavigator.js
+++ routes/MainNavigator.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useContext } from "react";
 
 import Home from "../screens/Home";
 import EditarProduto from "../screens/Editar";
@@ -7,16 +8,26 @@ import Login from "../screens/Login";
 import NovaCompra from "../screens/NovaCompra";
 import Register from "../screens/Register";
 
+import { AuthContext } from "../contexts/AuthContext";
+
 const Stack = createNativeStackNavigator();
 const MainNavigator = () => {
+  const { user } = useContext(AuthContext);
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="EditarProduto" component={EditarProduto} />
-        <Stack.Screen name="NovaCompra" component={NovaCompra} />
+        {!user.logado ? (
+          <>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Register" component={Register} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="EditarProduto" component={EditarProduto} />
+            <Stack.Screen name="NovaCompra" component={NovaCompra} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
