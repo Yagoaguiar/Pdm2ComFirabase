4a5370bea15b92060bea33b92299585393fab132
Yago Aguiar <95005910+Yagoaguiar@users.noreply.github.com>
2023-11-28 03:58:51 -0300

    validação de formulários foram feitas, close #39, close #43, close #9, close #35, close #25

--- screens/NovaCompra.js
+++ screens/NovaCompra.js
@@ -1,65 +1,80 @@
-import React, { useContext, useState } from 'react';
-import { View, ScrollView, StyleSheet, Text } from "react-native";
-import { Appbar, HelperText, Button, TextInput } from "react-native-paper";
-import { useForm } from 'react-hook-form';
+import React, { useContext } from 'react';
+import { View, ScrollView, StyleSheet, Text } from 'react-native';
+import { Appbar, HelperText, Button, TextInput } from 'react-native-paper';
+import { useForm, Controller } from 'react-hook-form';
 import { ListaCompraContext } from '../contexts/ListaDeCompraContext';
 
 const NovaCompra = ({ navigation }) => {
   const { adicionarItem } = useContext(ListaCompraContext);
 
-  const { control, handleSubmit, formState: { errors } } = useForm({
-    defaultValues: {
-      novoProduto: '',
-      novaQuantidade: '',
-    },
-  });
-
-  const [produto, setProduto] = useState('');
-  const [quantidade, setQuantidade] = useState('');
-
-  const handleAdicionarItem = () => {
-    adicionarItem(produto, quantidade);
-    navigation.navigate('Home');
-  };
+  const { control, handleSubmit, formState: { errors } } = useForm();
 
   const produtoRole = {
-    required: { value: true, message: "Produto é obrigatório" },
-    minLength: { value: 1, message: "Produto deve ter pelo menos 1 letra" },
+    required: { value: true, message: 'Produto é obrigatório' },
+    minLength: { value: 2, message: 'Produto deve ter pelo menos 2 letras' },
+    pattern: {
+      value: /^[A-Za-zÀ-ú\s]+$/, 
+      message: 'Por favor, insira apenas letras no campo Produto',
+    },
   };
-
   const quantidadeRole = {
     required: { value: true, message: 'Quantidade é obrigatória' },
     min: { value: 1, message: 'A quantidade deve ser maior que 0' },
+  };
+
+  const onSubmit = (data) => {
+    adicionarItem(data.novoProduto, data.novaQuantidade);
+    navigation.navigate('Home');
   };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.secondView}>
         <Text style={styles.title}>Adicionar novo produto</Text>
-        <TextInput
-          label="Produto"
-          value={produto}
-          onChangeText={(text) => setProduto(text)}
-          error={errors.novoProduto ? true : false}
+        <Controller
+          control={control}
+          rules={produtoRole}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View>
+              <TextInput
+                label="Produto"
+                value={value}
+                onBlur={onBlur}
+                onChangeText={(text) => onChange(text)}
+              />
+              {errors.novoProduto && (
+                <HelperText type="error">{errors.novoProduto.message}</HelperText>
+              )}
+            </View>
+          )}
+          name="novoProduto"
+          defaultValue=""
         />
-        {errors.novoProduto && (
-          <HelperText type="error">{errors.novoProduto.message}</HelperText>
-        )}
-        <TextInput
-          label="Quantidade"
-          keyboardType="numeric"
-          value={quantidade}
-          onChangeText={(text) => setQuantidade(text)}
-          error={errors.novaQuantidade ? true : false}
+        <Controller
+          control={control}
+          rules={quantidadeRole}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View>
+              <TextInput
+                label="Quantidade"
+                keyboardType="numeric"
+                value={value}
+                onBlur={onBlur}
+                onChangeText={(text) => onChange(text)}
+              />
+              {errors.novaQuantidade && (
+                <HelperText type="error">{errors.novaQuantidade.message}</HelperText>
+              )}
+            </View>
+          )}
+          name="novaQuantidade"
+          defaultValue=""
         />
-        {errors.novaQuantidade && (
-          <HelperText type="error">{errors.novaQuantidade.message}</HelperText>
-        )}
         <Button
           mode="contained"
           color="blue"
           style={styles.Button}
-          onPress={handleSubmit(handleAdicionarItem)}
+          onPress={handleSubmit(onSubmit)}
         >
           Adicionar
         </Button>
